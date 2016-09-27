255c8b5a7488532458d7587dd2c78cbc2ce295fc
Jon Skarpeteig <jskarpet@cisco.com>
2016-09-27 15:15:49 +0200

    Add a ton of generic tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 # Class: lysaker_monitored
-# ===========================
+#===========================
 #
 # Full description of class lysaker_monitored here.
 #
 # Parameters
-# ----------
+#----------
 #
 # * `sample parameter`
 #   Explanation of what this parameter affects and what it defaults to.
@@ -12,9 +12,7 @@
 #
 class lysaker_monitored (
   $influxdb_hostname = $::lysaker_monitored::params::influxdb_hostname,
-  $influxdb_port = $::lysaker_monitored::params::influxdb_port,
-) inherits ::lysaker_monitored::params {
-
+  $influxdb_port     = $::lysaker_monitored::params::influxdb_port,) inherits ::lysaker_monitored::params {
   # validate parameters here
 
   class { '::collectd':
@@ -26,7 +24,13 @@ class lysaker_monitored (
 
   collectd::plugin::network::server { $lysaker_monitored::influxdb_hostname: port => $lysaker_monitored::influxdb_port, }
 
-  class { '::collectd::plugin::conntrack': }
+  class { 'collectd::plugin::cgroups':
+    ignore_selected => true,
+    cgroups         => ['array', 'of', 'paths']
+  }
+
+  class { '::collectd::plugin::conntrack':
+  }
 
   class { '::collectd::plugin::cpu':
     reportbystate    => true,
@@ -34,10 +38,67 @@ class lysaker_monitored (
     valuespercentage => true,
   }
 
-  class { '::collectd::plugin::memory': }
+  class { 'collectd::plugin::cpufreq':
+  }
 
-  class { '::collectd::plugin::interface':
-    interfaces     => ['eth0', 'lo'],
+  class { 'collectd::plugin::df':
+    fstypes        => ['nfs', 'tmpfs', 'autofs', 'gpfs', 'proc', 'devpts'],
     ignoreselected => true,
   }
+
+  class { 'collectd::plugin::disk':
+    disks          => ['/^dm/'],
+    ignoreselected => true,
+    udevnameattr   => 'DM_NAME',
+  }
+
+  class { 'collectd::plugin::ethstat':
+    interfaces => ['eth0', 'eth1', 'eno160'],
+    maps       => ['"rx_csum_offload_errors" "if_rx_errors" checksum_offload"', '"multicast" "if_multicast"'],
+    mappedonly => false,
+  }
+
+  class { 'collectd::plugin::fhcount':
+    valuesabsolute   => true,
+    valuespercentage => false,
+  }
+
+  class { 'collectd::plugin::load':
+  }
+
+  class { 'collectd::plugin::logfile':
+    log_level => 'warning',
+    log_file  => '/var/log/collected.log'
+  }
+
+  class { '::collectd::plugin::memory':
+  }
+
+  class { 'collectd::plugin::netlink':
+    interfaces        => ['All'],
+    verboseinterfaces => ['ppp0'],
+    qdiscs            => ['"eth0" "pfifo_fast-1:0"', '"ppp0"'],
+    classes           => ['"ppp0" "htb-1:10"'],
+    filters           => ['"ppp0" "u32-1:0"'],
+    ignoreselected    => false,
+  }
+
+  class { 'collectd::plugin::ntpd':
+    host           => 'localhost',
+    port           => 123,
+    reverselookups => false,
+    includeunitid  => false,
+  }
+
+  class { 'collectd::plugin::ping':
+    hosts => ['vg.no'],
+  }
+
+  class { 'collectd::plugin::uptime':
+  }
+
+  class { 'collectd::plugin::vmem':
+    verbose => true,
+  }
+
 }
