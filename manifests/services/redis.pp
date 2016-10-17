dd31874cada42ed2ee3cd4494c88c27ce48ce5bf
Jon Skarpeteig <jskarpet@cisco.com>
2016-10-17 15:42:20 +0200

    Lint fix

--- manifests/services/redis.pp
+++ manifests/services/redis.pp
@@ -1,10 +1,10 @@
 #
 class lysaker_monitored::services::redis {
-  class { 'collectd::plugin::redis':
+  class { '::collectd::plugin::redis':
     nodes => {
       'localhost' => {
         'host' => 'localhost',
       },
-    }
+    },
   }
 }
