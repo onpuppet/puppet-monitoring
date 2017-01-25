d7c9b8d148ce0856d736c353de7e1f802e9c05dc
Jon Skarpeteig <jskarpet@cisco.com>
2017-01-25 11:29:08 +0100

    Update elasticsearch checks

--- manifests/monitoring/sensu/elasticsearch.pp
+++ manifests/monitoring/sensu/elasticsearch.pp
@@ -5,13 +5,20 @@ class monitoring::monitoring::sensu::elasticsearch (
   $plugins_location = '/opt/sensu/embedded/bin/',
 ) {
 
+  package { ['gcc', 'g++', 'make']:
+    ensure => 'present',
+  }
   package { 'sensu-plugins-elasticsearch':
     ensure   => 'present',
     provider => sensu_gem,
+    require  => [Package['gcc'], Package['g++'], Package['make']],
   }
 
   sensu::check { 'es-circuit-breakers':
     command => "${plugins_location}check-es-circuit-breakers.rb",
+  }
+  sensu::check { 'es-cluster-health':
+    command => "${plugins_location}check-es-cluster-health.rb",
   }
   sensu::check { 'es-cluster-status':
     command => "${plugins_location}check-es-cluster-status.rb",
@@ -20,15 +27,15 @@ class monitoring::monitoring::sensu::elasticsearch (
     command => "${plugins_location}check-es-file-descriptors.rb",
   }
   sensu::check { 'es-heap':
-    command => "${plugins_location}check-es-heap.rb --percentage --warn 80 --crit 90",
+    command => "${plugins_location}check-es-heap.rb --percentage --warn 70 --crit 80",
   }
   sensu::check { 'es-indexes':
     command => "${plugins_location}check-es-indexes.rb --cluster elasticsearch",
   }
+  sensu::check { 'es-indices-sizes':
+    command => "${plugins_location}check-es-indices-sizes.rb",
+  }
   sensu::check { 'es-node-status':
     command => "${plugins_location}check-es-node-status.rb",
-  }
-  sensu::check { 'es-shard-allocation-status':
-    command => "${plugins_location}check-es-shard-allocation-status.rb",
   }
 }
