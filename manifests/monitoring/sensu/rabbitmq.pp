1303812c9ba0caa763c616a5768d17a502751fe5
Jon Skarpeteig <jskarpet@cisco.com>
2017-01-27 16:19:41 +0100

    Fix issue with RabbitMQ management not present

--- manifests/monitoring/sensu/rabbitmq.pp
+++ manifests/monitoring/sensu/rabbitmq.pp
@@ -1,20 +1,12 @@
 # Class: monitoring::monitoring::rabbitmq
-# ======================================
 #
-class monitoring::monitoring::sensu::rabbitmq (
-  $plugins_location = '/opt/sensu/embedded/bin/',
-) {
-
+class monitoring::monitoring::sensu::rabbitmq ($plugins_location = '/opt/sensu/embedded/bin/',) {
   if !defined(Package['g++']) {
-    package { 'g++':
-      ensure => 'present',
-    }
+    package { 'g++': ensure => 'present', }
   }
 
   if !defined(Package['make']) {
-    package { 'make':
-      ensure => 'present',
-    }
+    package { 'make': ensure => 'present', }
   }
 
   package { 'sensu-plugins-rabbitmq':
@@ -23,22 +15,17 @@ class monitoring::monitoring::sensu::rabbitmq (
     require  => [Package['make'], Package['g++']],
   }
 
-  sensu::check { 'rabbitmq-alive':
-    command => "${plugins_location}check-rabbitmq-alive.rb",
-  }
-  sensu::check { 'rabbitmq-amqp-alive':
-    command => "${plugins_location}check-rabbitmq-amqp-alive.rb",
-  }
-  sensu::check { 'rabbitmq-cluster-health':
-    command => "${plugins_location}check-rabbitmq-cluster-health.rb",
-  }
-  sensu::check { 'rabbitmq-messages':
-    command => "${plugins_location}check-rabbitmq-messages.rb",
-  }
-  sensu::check { 'rabbitmq-network-partitions':
-    command => "${plugins_location}check-rabbitmq-network-partitions.rb",
-  }
-  sensu::check { 'rabbitmq-node-health':
-    command => "${plugins_location}check-rabbitmq-node-health.rb",
+  sensu::check { 'rabbitmq-amqp-alive': command => "${plugins_location}check-rabbitmq-amqp-alive.rb", }
+
+  if $::rabbitmq_management_present {
+    sensu::check { 'rabbitmq-alive': command => "${plugins_location}check-rabbitmq-alive.rb", }
+
+    sensu::check { 'rabbitmq-cluster-health': command => "${plugins_location}check-rabbitmq-cluster-health.rb", }
+
+    sensu::check { 'rabbitmq-messages': command => "${plugins_location}check-rabbitmq-messages.rb", }
+
+    sensu::check { 'rabbitmq-network-partitions': command => "${plugins_location}check-rabbitmq-network-partitions.rb", }
+
+    sensu::check { 'rabbitmq-node-health': command => "${plugins_location}check-rabbitmq-node-health.rb", }
   }
 }
